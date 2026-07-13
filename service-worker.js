4f688a5ee7c1f937654429bb8b51b84deb989dbe
vvarzea <vvarzea@gmail.com>
2026-07-13 07:52:17 +0000

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 /* VanBerto's PWA Service Worker (offline-first) */
-const CACHE_VERSION = "vanbertos-v14";
+const CACHE_VERSION = "vanbertos-v15";
 const CORE_ASSETS = [
   "./",
   "./index.html",
